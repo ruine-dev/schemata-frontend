74ba6d4e5742baaf614a789101c94da332b89e0f
Faqih Muntashir <itsfaqih@gmail.com>
2023-01-02 00:21:10 +0700

    chore: update information version to 0.1.2

--- src/components/InformationDialog.tsx
+++ src/components/InformationDialog.tsx
@@ -12,7 +12,7 @@ export function InformationDialog({ trigger }: InformationDialogProps) {
         <table>
           <tr>
             <td className="py-1 pr-8 text-slate-500">Version</td>
-            <td className="py-1 text-slate-800">Alpha 0.1.0</td>
+            <td className="py-1 text-slate-800">Alpha 0.1.2</td>
           </tr>
           <tr>
             <td className="py-1 pr-8 text-slate-500">Last updated</td>
