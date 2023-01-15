74e0c35cbdb9d65a4b3f6e77c1c5bcd16410f1b8
Faqih Muntashir <itsfaqih@gmail.com>
2023-01-15 20:02:07 +0700

    test: update column using double click for entry

--- cypress/e2e/column/updating-column.cy.ts
+++ cypress/e2e/column/updating-column.cy.ts
@@ -206,6 +206,100 @@ describe('updating column', () => {
       });
     });
   });
+
+  context('can edit column by double clicking column', () => {
+    beforeEach(() => {
+      cy.getBySel('column').dblclick();
+
+      cy.focused().clear().type('foo');
+    });
+
+    afterEach(() => {
+      cy.getBySel('column-name').should('have.text', 'foo');
+    });
+
+    context('without changing data type', () => {
+      afterEach(() => {
+        cy.getBySel('column-type').should('have.text', 'VARCHAR');
+      });
+
+      context('left primary key unchecked', () => {
+        afterEach(() => {
+          cy.getBySel('column-key').should('not.have.text', 'Primary key');
+        });
+
+        it('pressing "Enter" to submit', () => {
+          cy.focused().type('{enter}');
+        });
+
+        it('pressing "Escape" to submit', () => {
+          cy.focused().type('{esc}');
+        });
+      });
+
+      context('checked primary key', () => {
+        beforeEach(() => {
+          cy.getBySel('column-primary-key-checkbox').click();
+        });
+
+        afterEach(() => {
+          cy.getBySel('column-key').should('have.text', 'Primary key');
+        });
+
+        it('pressing "Enter" after change focus to name input to submit', () => {
+          cy.getBySel('column-name-textbox').type('{enter}');
+        });
+
+        it('pressing "Escape" to submit', () => {
+          cy.focused().type('{esc}');
+        });
+      });
+    });
+
+    context('with changing data type', () => {
+      beforeEach(() => {
+        cy.getBySel('column-type-combobox').click();
+
+        cy.getBySel('column-type-combobox-INTEGER').click();
+      });
+
+      afterEach(() => {
+        cy.getBySel('column-type').should('have.text', 'INTEGER');
+      });
+
+      context('left primary key unchecked', () => {
+        afterEach(() => {
+          cy.getBySel('column-key').should('not.have.text', 'Primary key');
+        });
+
+        it('pressing "Enter" to submit', () => {
+          cy.focused().type('{enter}');
+        });
+
+        it('pressing "Escape" to submit', () => {
+          cy.focused().type('{esc}');
+        });
+      });
+
+      context('checked primary key', () => {
+        beforeEach(() => {
+          cy.getBySel('column-primary-key-checkbox').click();
+        });
+
+        afterEach(() => {
+          cy.getBySel('column-key').should('have.text', 'Primary key');
+        });
+
+        it('pressing "Enter" after change focus to name input to submit', () => {
+          cy.getBySel('column-name-textbox').type('{enter}');
+        });
+
+        it('pressing "Escape" to submit', () => {
+          cy.focused().type('{esc}');
+        });
+      });
+    });
+  });
 });
 
 export {};
