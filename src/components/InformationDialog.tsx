4acc1e3fed0895ed6172d500eaf05fc3c36a6742
Faqih Muntashir <itsfaqih@gmail.com>
2023-01-26 21:33:29 +0700

    chore: update information version to 0.23.1

--- src/components/InformationDialog.tsx
+++ src/components/InformationDialog.tsx
@@ -11,8 +11,8 @@ export function InformationDialog({ trigger }: InformationDialogProps) {
     <BaseDialog title="Information" trigger={trigger} closeOnInteractOutside>
       <div className="max-w-[24rem] pt-4 pb-2">
         <dl className="flex flex-col gap-y-4">
-          <DescListItem label="Version" value="Alpha 0.23.0" />
-          <DescListItem label="Last updated" value="25 January 2023" />
+          <DescListItem label="Version" value="Alpha 0.23.1" />
+          <DescListItem label="Last updated" value="26 January 2023" />
           <DescListItem
             label="Created by"
             value={
